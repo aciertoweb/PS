88551db55835a4107193c1a0d00ed6c7b3ed6e09
aciertoweb <aciertoweb@gmail.com>
2016-10-08 10:54:05 +0200

    done

---
+++ test.ps1
@@ -0,0 +1,15 @@
+function create_datetimestamp ()
+{
+    get-date -Format yyyyMMdd-hh:mm:ss
+}
+
+create_datetimestamp
+
+$i = 0;
+$max = 4;
+while ($i -lt $max)
+{
+    echo "JUAAAn";
+    $i++;
+
+}
